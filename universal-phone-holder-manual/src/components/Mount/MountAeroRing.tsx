db57e8dddb9adc5c3689f06b81494f6a1bbb91e5
Jan Chen <jan_201@hotmail.de>
2024-10-06 05:44:17 +0200

    text and image for aero mount v1

--- universal-phone-holder-manual/src/components/Mount/MountAeroRing.tsx
+++ universal-phone-holder-manual/src/components/Mount/MountAeroRing.tsx
@@ -3,6 +3,7 @@ import { Vector3 } from 'three'
 import { Paths } from '../../constants/Paths'
 import { ModelAndTextLayout } from '../Layouts/ModelAndTextLayout'
 import { StlView } from '../Layouts/StlView'
+import { ImagePopover } from '../Tooltip/ImagePopover'
 
 export const MountAeroRing : FunctionComponent = () => {
     return <ModelAndTextLayout 
@@ -19,7 +20,8 @@ export const MountAeroRing : FunctionComponent = () => {
         }
         text={
             <>
-                tset
+                Wrap the <ImagePopover text='aero mount ring' id='aero-mount-ring-v1' imagePath='./img/aero-mount-ring-v1.png' />
+                around the handlebar.
             </>
         }
         previousStepPath={Paths.MountAeroHolderPath}
